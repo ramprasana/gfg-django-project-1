338d63c96b16fe4383341a44770aa4e978002d1a
Ram Prasanna <ramprasana@yahoo.com>
2023-06-21 09:25:49 -0700

    person app

--- djangobasic1/djangobasic1/wsgi.py
+++ djangobasic1/djangobasic1/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobasic1.settings')
 
 application = get_wsgi_application()
+
+# gunicorn django_proj.djangobasic1.djangobasic1.wsgi:application
